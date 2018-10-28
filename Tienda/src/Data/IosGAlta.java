299c595dd8cdafa74df9971f752f1edf3aef53c5
Prometeo <Prometeo@10.208.9.109>
2018-10-28 16:51:59 -0500

    emmanuel 28-octubre

---
+++ Tienda/src/Data/IosGAlta.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author Prometeo
+ */
+public class IosGAlta extends Ios{
+    private String tipoDeSeguro;
+    private String adicionales;
+    private String colorEstuche;
+
+    public IosGAlta(String tipoDeSeguro, String adicionales, String colorEstuche, String nombreComercial, float version, int anioLanzamiento) {
+        super(nombreComercial, version, anioLanzamiento);
+        setTipoDeSeguro(tipoDeSeguro);
+        setAdicionales(adicionales);
+        setColorEstuche(colorEstuche);
+    }
+
+    public IosGAlta(String tipoDeSeguro, String adicionales, String colorEstuche, String nombreComercial, float version, int anioLanzamiento, float pantalla, boolean espacialesParaLectura, String referencia, String color, String marca, float precio) {
+        super(nombreComercial, version, anioLanzamiento, pantalla, espacialesParaLectura, referencia, color, marca, precio);
+        setTipoDeSeguro(tipoDeSeguro);
+        setAdicionales(adicionales);
+        setColorEstuche(colorEstuche);
+    }
+
+    public IosGAlta(String tipoDeSeguro, String adicionales, String colorEstuche, String nombreComercial, float version, int anioLanzamiento, float pantalla, boolean espacialesParaLectura, String referencia) {
+        super(nombreComercial, version, anioLanzamiento, pantalla, espacialesParaLectura, referencia);
+        setTipoDeSeguro(tipoDeSeguro);
+        setAdicionales(adicionales);
+        setColorEstuche(colorEstuche);
+    }
+
+    public IosGAlta(String tipoDeSeguro, String adicionales, String colorEstuche) {
+        setTipoDeSeguro(tipoDeSeguro);
+        setAdicionales(adicionales);
+        setColorEstuche(colorEstuche);
+    }
+    public IosGAlta() {
+        setTipoDeSeguro("valor no asignado");
+        setAdicionales("valor no asignado");
+        setColorEstuche("valor no asignado");
+    }
+       
+    
+    public void setTipoDeSeguro(String tipoDeSeguro){
+    this.tipoDeSeguro=tipoDeSeguro;
+    }
+    public void setAdicionales(String adicionales){
+    this.adicionales=adicionales;
+    }
+    public void setColorEstuche(String colorEstuche){
+    this.colorEstuche=colorEstuche;
+    }
+    
+    public String getTipoDeSeguro(){
+    return this.tipoDeSeguro;
+    }
+    public String getAdicionales(){
+    return this.adicionales;
+    }
+    public String getColorEstuche(){
+    return this.colorEstuche;
+    }
+    
+    @Override
+    public String toString(){    
+    String cadena1=super.toString();
+    String cadena2="\nel celular tiene un incluido un tipo de seguro :"+getTipoDeSeguro()+"\naccesorios adiconales incluidos : "+getAdicionales()+"\nel color del estuche incluido es:"+getColorEstuche();
+    return cadena1+cadena2;
+    }
+    
+    
+    
+            
+}
---
+++ Tienda/src/Data/VersionNueva.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author Prometeo
+ */
+public class VersionNueva extends Android{
+    private boolean dobleCamara;
+    private String accesorios;
+    private boolean recibeBono;
+    
+    public VersionNueva(){
+        setDobleCamara(false);
+        setAccesorios("no se han encontrado accesorios");
+        setRecibeBono(false);
+    }
+    
+    
+    public VersionNueva(boolean dobleCamara, String accesorios, boolean recibeBono) {
+        setDobleCamara(dobleCamara);
+        setAccesorios(accesorios);
+        setRecibeBono(recibeBono);
+    }
+    public VersionNueva(boolean dobleCamara, String accesorios, boolean recibeBono, float versionAndorid, int numeroDeSalidas, boolean audifonos) {
+        super(versionAndorid, numeroDeSalidas, audifonos);
+        setDobleCamara(dobleCamara);
+        setAccesorios(accesorios);
+        setRecibeBono(recibeBono);
+    }
+     public VersionNueva(boolean dobleCamara, String accesorios, boolean recibeBono, float versionAndorid, int numeroDeSalidas, boolean audifonos, float pantalla, boolean espacialesParaLectura, String referencia) {
+        super(versionAndorid, numeroDeSalidas, audifonos, pantalla, espacialesParaLectura, referencia);
+        setDobleCamara(dobleCamara);
+        setAccesorios(accesorios);
+        setRecibeBono(recibeBono);
+    }
+
+    public VersionNueva(boolean dobleCamara, String accesorios, boolean recibeBono, float versionAndorid, int numeroDeSalidas, boolean audifonos, float pantalla, boolean espacialesParaLectura, String referencia, String color, String marca, float precio) {
+        super(versionAndorid, numeroDeSalidas, audifonos, pantalla, espacialesParaLectura, referencia, color, marca, precio);
+        setDobleCamara(dobleCamara);
+        setAccesorios(accesorios);
+        setRecibeBono(recibeBono);
+    }
+    
+public void setDobleCamara(boolean dobleCamara){
+this.dobleCamara=dobleCamara;
+}
+public void setAccesorios(String accesorios){
+this.accesorios=accesorios;
+}
+public void setRecibeBono(boolean recibeBono){
+this.recibeBono=recibeBono;
+}
+public boolean getDobleCamara(){
+return this.dobleCamara;
+}
+public String getAccesorios(){
+return this.accesorios;
+}
+public boolean getRecibeBono(){
+return this.recibeBono;
+}
+
+@Override
+public String toString(){
+String cadena1=super.toString();
+String cadena="/nel celular tiene doble camar: "+getDobleCamara()+"/ncontiene los siguentes accesorios: "+getAccesorios()+"/nrecibe bono de regalo?: "+getRecibeBono(); 
+return cadena1+cadena;
+}
+    
+}
